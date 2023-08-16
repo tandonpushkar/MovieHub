88abeec423d550dbb38533049b570cff8e14e03a
tandonpushkar <tandonpushkar@gmail.com>
2023-08-16 17:23:34 +0530

    android changes

--- app/screens/SingleMovieScreen/single-screen.tsx
+++ app/screens/SingleMovieScreen/single-screen.tsx
@@ -1,5 +1,5 @@
 import React, {memo} from 'react';
-import {View} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import {AppHeader, Container, CustomText, ImageViewFallback} from '@components';
 import {colors} from '@theme';
 
@@ -19,37 +19,39 @@ export const SingleMovie: any = memo((props: SingleMovieProps) => {
       backgroundColor={colors.appBackground}
       containerStyle={styles.container}>
       <AppHeader title={'back'} />
-      <ImageViewFallback
-        source={{
-          uri: `${IMAGE_BASE_URL_ORIGINAL}${movieItem?.backdrop_path}`,
-        }}
-        resizeMode={'contain'}
-        style={styles.img_style}
-      />
-      <View style={styles.cnt_1}>
-        <CustomText style={styles.txt_2}>
-          Release Date{'  '}
-          <CustomText style={styles.txt_3}>
-            {moment(movieItem?.release_date)?.format('DD MMM YY')}
+      <ScrollView>
+        <ImageViewFallback
+          source={{
+            uri: `${IMAGE_BASE_URL_ORIGINAL}${movieItem?.backdrop_path}`,
+          }}
+          resizeMode={'contain'}
+          style={styles.img_style}
+        />
+        <View style={styles.cnt_1}>
+          <CustomText style={styles.txt_2}>
+            Release Date{'  '}
+            <CustomText style={styles.txt_3}>
+              {moment(movieItem?.release_date)?.format('DD MMM YY')}
+            </CustomText>
           </CustomText>
-        </CustomText>
 
-        <CustomText style={styles.txt_2}>
-          Rating{'  '}
-          <CustomText style={styles.txt_4}>
-            {movieItem?.vote_average?.toFixed(1)}
+          <CustomText style={styles.txt_2}>
+            Rating{'  '}
+            <CustomText style={styles.txt_4}>
+              {movieItem?.vote_average?.toFixed(1)}
+            </CustomText>
+            /10
           </CustomText>
-          /10
-        </CustomText>
-      </View>
-      <View style={styles.cnt_2}>
-        <CustomText numberOfLines={2} style={styles.title_txt}>
-          {movieItem?.title}
-        </CustomText>
-        <CustomText style={styles.subTitle_txt}>
-          {movieItem?.overview}
-        </CustomText>
-      </View>
+        </View>
+        <View style={styles.cnt_2}>
+          <CustomText numberOfLines={2} style={styles.title_txt}>
+            {movieItem?.title}
+          </CustomText>
+          <CustomText style={styles.subTitle_txt}>
+            {movieItem?.overview}
+          </CustomText>
+        </View>
+      </ScrollView>
     </Container>
   );
 });
